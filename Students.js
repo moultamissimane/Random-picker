5a84c80e11f1bebcb45592bf459edab59ec71320
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-09-26 01:00:16 +0100

    edit add student

--- Students.js
+++ Students.js
@@ -6,16 +6,16 @@ const log = (ele) => {
 
 const getInput = () => {
   let fullname = document.getElementById("fullname").value;
-  let brief = document.getElementById("brief").value;
+  let subject = document.getElementById("brief").value;
   let element = {
     id: `${Math.floor(Math.random() * 10)}${Date.now()}`,
     fullName: fullname,
-    brief: brief,
+    subject: subject,
     status: "notChoosed",
     date: "",
   };
   console.log(element);
-  addElement(element);
+  addElements(element);
 };
 document.querySelector("#btnAdd").addEventListener("click", getInput);
 
@@ -60,16 +60,9 @@ const deleteElements = async (element) => {
 const render = (elements) => {
   tableContent.innerHTML = "";
   var newTr = document.createElement("tr");
-  newTr.map((e, idx) => {
-    tableContent.innerHTML += `
+  elements.map((e, idx) => {
+    tableContent.innerHTML = `
     <tr class="p-3 border-t border-black">
-      <td class="px-4 py-4 text-base border-b border-gray-200 bg-white ">
-         <div class="flex items-center">
-            <div class="flex-shrink-0">
-                <p>${e.id} </p>
-            </div>
-      </div>
-      </td>
       <td class="px-4 py-4 text-base border-b border-gray-200 bg-white ">
          <div class="flex items-center">
             <div class="flex-shrink-0">
@@ -101,13 +94,14 @@ const render = (elements) => {
       <td class="px-4 py-4 border-b border-gray-200 bg-white text-base">
         <div class="flex items-center">
             <div class="flex-shrink-0">
-                <button id="${e.id} class="bg-red-400 hover:bg-red-600 text-white font-bold py-1 px-4 rounded">
+                <button id="${
+                  e.id
+                } class="bg-red-400 hover:bg-red-600 text-white font-bold py-1 px-4 rounded">
                     Repeat
                 </button>
             </div>
         </div>
       </td>
-     
     </tr>
     `;
     tableContent.appendChild(newTr);
@@ -115,3 +109,48 @@ const render = (elements) => {
   console.log(elements);
   addEventListenerToReset();
 };
+
+const addEventListenerToReset = () => {
+  const buttonRepeat = document.querySelectorAll(".buttonRepeat");
+  buttonRepeat.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      resetElement(e.target.id);
+    });
+  });
+};
+
+// update the element from data input to json
+const updateElement = async (element) => {
+  const response = await fetch(`http://localhost:3000/elements/${element.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(element),
+  });
+  const data = await response.json();
+  console.log(data);
+  render(fetchElements());
+};
+const resetElement = async (id) => {
+  const response = await fetch(`http://localhost:3000/elements/${id}`);
+  const data = await response.json();
+  data.status = "notPicked";
+  data.date = "";
+  updateElement(data);
+};
+
+const changeDate = async () => {
+  let elements = await fetchElements();
+  elements.map((e) => {
+    var idElement = document.getElementById("spinnerWinnerId").value;
+    console.log(idElement);
+    if (e.id === idElement) {
+      e.date = document.getElementById("datePickerInput").value;
+      e.status = "choosed";
+      updateElement(e);
+    }
+  });
+};
+
+document.querySelector("#ChooseBtn").addEventListener("click", changeDate);
--- picker.js
+++ picker.js
@@ -2,13 +2,13 @@ var picker = document.getElementById("#picker");
 let random;
 
 const fetchData = async () => {
-  const response = await fetch("http://127.0.0.1:5500/Data");
+  const response = await fetch(" http://localhost:3000/Data");
   const data = await response.json();
   return data;
 };
 
 const render = async (elements) => {
-  picker.innerHTML = "";
+  spinner.innerHTML = "";
   let arr = [];
   elements.forEach((element) => {
     if (element.status === "notChoosed") {
@@ -16,57 +16,78 @@ const render = async (elements) => {
     }
   });
 
-    random = arr[Math.floor(Math.random() * arr.length)];   
-    console.log(random);
-    const element = elements.find((element) => element.id === random);
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <div class="card">
-    <div class="card-body">
-        <h5 class="card-title">${element.name}</h5>
-        <p class="card-text">${element.description}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-    </div>
-    `;
-    picker.appendChild(div);
+//arr random
+  random = arr[Math.floor(Math.random() * arr.length)];
+  let randomElement = elements.find((element) => element.id === random);
+  randomElement.style = "transform: scale(1.3)";
+  await elements.map((e) => {
+    if (e.status === "notChoosed") {
+      var newSpin = document.createElement("div");
+      newSpin.classList.add("scale-50");
+      newSpin.classList.add("w-48");
+      newSpin.classList.add("nodeChildrenSpinner");
+
+      newSpin.innerHTML = `
+            <div class="flex w-48 rounded-lg px-4 h-48 scale-50 text-base flex-col shadow-sm p-3 ${
+              e.id == random ? "bg-red-600 text-white h-52 -mt-2" : "bg-white"
+            } ${e.status != "notChoosed" ? "bg-gray-200" : ""}" id="id_${
+        e.id
+      }" >
+            <h3 class="text-2xl font-semibold items-baseline whitespace-nowrap">${
+              e.fullName
+            }</h3>
+            <p>${e.brief}</p>
+            ${
+              e.status != "notChoosed"
+                ? '<hr class="border-white"/><p class="text-black mt-2">' +
+                  e.date +
+                  "</p>"
+                : ""
+            }
+            </div>
+            `;
+      document.getElementById("spinnerWinner").innerHTML =
+        randomElement.fullName;
+      document.getElementById("spinnerWinnerId").value = randomElement.id;
+      spinner.appendChild(newSpin);
+    }
+  });
+  scrollToElement(randomElement.id);
 };
 
-//   //arr random
-//   random = arr[Math.floor(Math.random() * arr.length)];
-//   let randomElement = elements.find((element) => element.id === random);
-//   randomElement.style = "transform: scale(1.3)";
-//   await elements.map((e) => {
-//     if (e.status === "notChoosed") {
-//       var newSpin = document.createElement("div");
-//       newSpin.classList.add("scale-50");
-//       newSpin.classList.add("w-48");
-//       newSpin.classList.add("nodeChildrenSpinner");
+const scrollToElement = (id) => {
+  let childrenPos = document.getElementById("id_" + id).offsetLeft;
+  let parentPos = document.getElementById("pickerg").offsetLeft;
 
-//       newSpin.innerHTML = `
-//             <div class="flex w-48 rounded-lg px-4 h-48 scale-50 text-base flex-col shadow-sm p-3 ${
-//               e.id == random ? "bg-red-600 text-white h-52 -mt-2" : "bg-white"
-//             } ${e.status != "notChoosed" ? "bg-gray-200" : ""}" id="id_${
-//         e.id
-//       }" >
-//             <h3 class="text-2xl font-semibold items-baseline whitespace-nowrap">${
-//               e.fullName
-//             }</h3>
-//             <p>${e.brief}</p>
-//             ${
-//               e.status != "notChoosed"
-//                 ? '<hr class="border-white"/><p class="text-black mt-2">' +
-//                   e.date +
-//                   "</p>"
-//                 : ""
-//             }
-//             </div>
-//             `;
-//       document.getElementById("spinnerWinner").innerHTML =
-//         randomElement.fullName;
-//       document.getElementById("spinnerWinnerId").value = randomElement.id;
-//       spinner.appendChild(newSpin);
-//     }
-//   });
-//   scrollToElement(randomElement.id);
-// };
+  picker.scrollTo({
+    left: childrenPos - parentPos - 150,
+    behavior: "smooth",
+  });
+};
+
+picker.addEventListener("scroll", () => {
+  let nodeChildren = document.querySelectorAll("nodeChildrenSpinner");
+  var audio = new Audio("./Mouse.mp3");
+  nodeChildren.forEach((e) => {
+    if (e.offsetLeft < picker.scrollLeft + picker.clientWidth) {
+      //scale
+      audio.play();
+      e.style.transform = `scale(0.2)`;
+      e.style.transition = `transform 0.5s`;
+    } else {
+      //scale
+      e.style.transform = `scale(0.9)`;
+      e.style.transition = `transform 0.5s`;
+    }
+  });
+});
+
+//onclick rendomize
+var elements = fetchElements();
+
+//add event listener to randomize button
+const randomizeButton = document.querySelector("#btnSpinner");
+randomizeButton.addEventListener("click", fetchElements);
+
+fetchElements();
+document.querySelector("#btnAdd").addEventListener("click", fetchElements);
